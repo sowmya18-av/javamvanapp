5b98050311aa158714eb0b831758b671ce92e4dc
sowmya18-av <sowmyaangadiv18@gmail.com>
2022-05-05 08:02:01 +0530

    deliver file

---
+++ scripts/deliver.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+ VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+ java -jar target/${NAME}-${VERSION}.jar
+~                                          
