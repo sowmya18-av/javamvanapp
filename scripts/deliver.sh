0bb4631889f0fb2a3938667839e193ce320b8a8a
sowmya18-av <103129555+sowmya18-av@users.noreply.github.com>
2022-05-05 09:15:34 +0530

    Update deliver.sh

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -2,4 +2,4 @@
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
  VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
  java -jar target/${NAME}-${VERSION}.jar
-~                                          
+                                       
